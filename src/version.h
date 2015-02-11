ad2c6f9bfbd60715366787e2ad30adeaae2bf4cc
jforthofer <jforthofer@gmail.com>
2015-02-11 10:59:15 -0700

    update versions for tags

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@
 
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION "2.5.0"
-#define SVN_VERSION "e30c62c79a443e884a1e758dfc1a7131f95bc3ca"
-#define RELEASE_DATE "01/23/2015"
+#define VERSION "2.5.1"
+#define SVN_VERSION "aee7dc211ff920db735bd5d76f0aac4fd6fddf17"
+#define RELEASE_DATE "02/11/2015"
 #endif /* VERSION_H */
