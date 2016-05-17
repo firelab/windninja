1482e3a93224b435296fa26593f1ab3f4224f8b6
nwagenbrenner <nwagenbrenner@gmail.com>
2016-05-17 14:01:19 -0600

    resample pdf output properly

--- src/ninja/ninjafoam.cpp
+++ src/ninja/ninjafoam.cpp
@@ -2751,8 +2751,8 @@ int NinjaFoam::WriteOutputFiles()
 			angTempGrid=NULL;
             OutputWriter output;
 
-			angTempGrid = new AsciiGrid<double> (AngleGrid.resample_Grid(input.shpResolution, AsciiGrid<double>::order0));
-			velTempGrid = new AsciiGrid<double> (VelocityGrid.resample_Grid(input.shpResolution, AsciiGrid<double>::order0));
+			angTempGrid = new AsciiGrid<double> (AngleGrid.resample_Grid(input.pdfResolution, AsciiGrid<double>::order0));
+			velTempGrid = new AsciiGrid<double> (VelocityGrid.resample_Grid(input.pdfResolution, AsciiGrid<double>::order0));
 
 			output.setDirGrid(*angTempGrid);
 			output.setSpeedGrid(*velTempGrid);
