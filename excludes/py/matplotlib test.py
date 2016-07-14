937c06f63b079789cf2ed5c5da08af288707c71c
tfinney9 <tfinney@fs.fed.us>
2016-07-14 16:01:12 -0600

    attempted to create an exclude file

---
+++ excludes/py/matplotlib test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 14:24:06 2016
+
+@author: tanner
+"""
+
+import matplotlib
+
+a=matplotlib.pyplot.scatter(0,1,c='r',marker='.')
+a=matplotlib.pyplot.scatter(1,2,c='r',marker='o')
+a=matplotlib.pyplot.scatter(2,3,c='r',marker='v')
+a=matplotlib.pyplot.scatter(3,4,c='r',marker='s')
+a=matplotlib.pyplot.scatter(4,5,c='r',marker='p')
+a=matplotlib.pyplot.scatter(5,6,c='r',marker="x")
