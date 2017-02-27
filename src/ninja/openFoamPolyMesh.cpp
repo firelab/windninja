690a01ea2235850a7fc1b2b255dd8906bcab03df
latwood <pianotocador@gmail.com>
2017-02-27 13:12:42 -0700

    added system folder file writing (controlDict, fvSchemes, fvSolutions, setFieldsDict). for issue #172 [ci skip]

--- src/ninja/openFoamPolyMesh.cpp
+++ src/ninja/openFoamPolyMesh.cpp
@@ -70,10 +70,10 @@ openFoamPolyMesh::openFoamPolyMesh(std::string outputPath, double nxcells, doubl
 
     x,y,z;
 
-    //stuff for other files
     transportPropertiesPath = outputPath+"constant/transportProperties";
     diffusivityConstant = 0.05;
 
+//values used for time directory. Stuff above is for constant directory, but sometimes used for other stuff below as well
     scalarPath = outputPath+"0/T";
     sourcePath = outputPath+"0/source";
     velocityPath = outputPath+"0/U";
@@ -82,6 +82,57 @@ openFoamPolyMesh::openFoamPolyMesh(std::string outputPath, double nxcells, doubl
 
     phiPath = outputPath+"0/phi";
 
+//values used for system directory
+    controlDictPath = outputPath + "system/controlDict";
+    fvSchemesPath = outputPath + "system/fvSchemes";
+    fvSolutionPath = outputPath + "system/fvSolution";
+    setFieldsDictPath = outputPath + "system/setFieldsDict";
+
+    //controlDict variables
+    application = "myScalarTransportFoam";
+    startFrom = "startTime";
+    startTime = "0";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    stopAt = "endTime";
+    endTime = "3600";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    deltaT = "1.0";
+    writeControl = "timeStep";
+    writeInterval = "300";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    purgeWrite = "0";       // I like a value of 0 for smoke transport
+    writeFormat = "ascii";
+    writePrecision = "10";
+    writeCompression = "uncompressed";
+    timeFormat = "general";
+    timePrecision = "10";
+    runTimeModifiable = "true";
+
+    //fvSchemes variables
+    ddtSchemes_default = "Euler";
+    gradSchemes_default = "Gauss linear";
+    divSchemes_default = "none";
+    divSchemes_divOfPhiAndT = "bounded Gauss upwind";
+    laplacianSchemes_default = "Gauss linear limited 0.333";
+    laplacianSchemes_laplacianOfDTandT = "Gauss linear corrected";
+    interpolationSchemes_default = "linear";
+    SnGradSchemes_default = "corrected";
+    SnGradSchemes_SnGradOfT = "limited 0.5";
+    fluxRequired_default = "no";
+    fluxRequired_T = true;
+
+    //fvSolution variables
+    solvers_T_solver = "PBiCG";
+    solvers_T_preconditioner = "DILU";
+    solvers_T_tolerance = "1e-06";
+    solvers_T_relTol = "0.2";
+    SIMPLE_nNonOrthogonalCorrectors = "0";
+
+    //setFieldsDict variables
+    defaultTvalue = "0";
+    defaultSourceValue = "0";
+    distributionType = "boxToCell";
+    boxMinCoordinates = "726019.742 5206748.293 1210";
+    boxMaxCoordinates = "726283 5207018.3 1440";
+    distributionTValue = "75";
+    distributionSourceValue = "75";
 
     writePolyMeshFiles("points", elem);
 
@@ -125,10 +176,10 @@ openFoamPolyMesh::openFoamPolyMesh(std::string outputPath, Mesh mesh, double xll
     nfaces = Axcells*ypoints+Aycells*xpoints+Azcells*zpoints;
     ninternalfaces = nfaces-2*(Axcells+Aycells+Azcells);
 
-    //stuff for other files
     transportPropertiesPath = outputPath+"constant/transportProperties";
     diffusivityConstant = 0.05;
 
+//values used for time directory. Stuff above is for constant directory, but sometimes used for other stuff below as well
     scalarPath = outputPath+"0/T";
     sourcePath = outputPath+"0/source";
     velocityPath = outputPath+"0/U";
@@ -140,6 +191,58 @@ openFoamPolyMesh::openFoamPolyMesh(std::string outputPath, Mesh mesh, double xll
     element elem(&mesh);
 
     phiPath = outputPath+"0/phi";
+
+//values used for system directory
+    controlDictPath = outputPath + "system/controlDict";
+    fvSchemesPath = outputPath + "system/fvSchemes";
+    fvSolutionPath = outputPath + "system/fvSolution";
+    setFieldsDictPath = outputPath + "system/setFieldsDict";
+
+    //controlDict variables
+    application = "myScalarTransportFoam";
+    startFrom = "startTime";
+    startTime = "0";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    stopAt = "endTime";
+    endTime = "3600";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    deltaT = "1.0";
+    writeControl = "timeStep";
+    writeInterval = "300";    // I feel like this varies a lot depending on the different situations. So I guess this is the default value
+    purgeWrite = "0";       // I like a value of 0 for smoke transport
+    writeFormat = "ascii";
+    writePrecision = "10";
+    writeCompression = "uncompressed";
+    timeFormat = "general";
+    timePrecision = "10";
+    runTimeModifiable = "true";
+
+    //fvSchemes variables
+    ddtSchemes_default = "Euler";
+    gradSchemes_default = "Gauss linear";
+    divSchemes_default = "none";
+    divSchemes_divOfPhiAndT = "bounded Gauss upwind";
+    laplacianSchemes_default = "Gauss linear limited 0.333";
+    laplacianSchemes_laplacianOfDTandT = "Gauss linear corrected";
+    interpolationSchemes_default = "linear";
+    SnGradSchemes_default = "corrected";
+    SnGradSchemes_SnGradOfT = "limited 0.5";
+    fluxRequired_default = "no";
+    fluxRequired_T = true;
+
+    //fvSolution variables
+    solvers_T_solver = "PBiCG";
+    solvers_T_preconditioner = "DILU";
+    solvers_T_tolerance = "1e-06";
+    solvers_T_relTol = "0.2";
+    SIMPLE_nNonOrthogonalCorrectors = "0";
+
+    //setFieldsDict variables
+    defaultTvalue = "0";
+    defaultSourceValue = "0";
+    distributionType = "boxToCell";
+    boxMinCoordinates = "726019.742 5206748.293 1210";
+    boxMaxCoordinates = "726283 5207018.3 1440";
+    distributionTValue = "75";
+    distributionSourceValue = "75";
 
     std::cout << "just finished constructor\n";
     writePolyMeshFiles("array", elem);
@@ -217,6 +320,7 @@ bool openFoamPolyMesh::writePolyMeshFiles(std::string pointWriteType, element el
     makeFoamFooter();
     fclose(fzout);
 
+    //write the velocities
     fzout = fopen(velocityPath.c_str(), "w");
     makeFoamHeader("volVectorField","U","0");
     printVelocity(pointWriteType, elem);
@@ -225,9 +329,38 @@ bool openFoamPolyMesh::writePolyMeshFiles(std::string pointWriteType, element el
 
     //fzout = fopen(phiPath.c_str(), "w");
     //makeFoamHeader("surfaceScalarField","phi","0");
-    //printPhi(elem);
+    //calculatePhi(elem);
     //makeFoamFooter();
     //fclose(fzout);
+
+//now create the system files
+    //write the controlDict file
+    fzout = fopen(controlDictPath.c_str(), "w");
+    makeFoamHeader("dictionary","controlDict","system");
+    writeControlDict();
+    makeFoamFooter();
+    fclose(fzout);
+
+    //write the fvSchemes file
+    fzout = fopen(fvSchemesPath.c_str(),"w");
+    makeFoamHeader("dictionary","fvSchemes","system");
+    writeFvSchemes();
+    makeFoamFooter();
+    fclose(fzout);
+
+    //write the fvSolution file
+    fzout = fopen(fvSolutionPath.c_str(),"w");
+    makeFoamHeader("dictionary","fvSolution","system");
+    writeFvSolution();
+    makeFoamFooter();
+    fclose(fzout);
+
+    //write the setFieldsDict file
+    fzout = fopen(setFieldsDictPath.c_str(),"w");
+    makeFoamHeader("dictionary","setFieldsDict","system");
+    writeSetFieldsDict();
+    makeFoamFooter();
+    fclose(fzout);
 
     return true;
 }
@@ -913,7 +1046,116 @@ void openFoamPolyMesh::printVelocity(std::string pointWriteType, element elem)
     }
 }
 
-void openFoamPolyMesh::printPhi(element elem)
+void openFoamPolyMesh::calculatePhi(element elem)
 {
     //not sure if we need this yet. Have it here just in case, just as a reminder.
+}
+
+void openFoamPolyMesh::writeControlDict()
+{
+    fprintf(fzout,"application     %s;\n\n",application.c_str());
+    fprintf(fzout,"startFrom       %s;\n\n",startFrom.c_str());
+    fprintf(fzout,"startTime       %s;\n\n",startTime.c_str());
+    fprintf(fzout,"stopAt          %s;\n\n",stopAt.c_str());
+    fprintf(fzout,"endTime         %s;\n\n",endTime.c_str());
+    fprintf(fzout,"deltaT          %s;\n\n",deltaT.c_str());
+    fprintf(fzout,"writeControl    %s;\n\n",writeControl.c_str());
+    fprintf(fzout,"writeInterval   %s;\n\n",writeInterval.c_str());
+    fprintf(fzout,"purgeWrite      %s;\n\n",purgeWrite.c_str());
+    fprintf(fzout,"writeFormat     %s;\n\n",writeFormat.c_str());
+    fprintf(fzout,"writePrecision  %s;\n\n",writePrecision.c_str());
+    fprintf(fzout,"writeCompression %s;\n\n",writeCompression.c_str());
+    fprintf(fzout,"timeFormat      %s;\n\n",timeFormat.c_str());
+    fprintf(fzout,"timePrecision   %s;\n\n",timePrecision.c_str());
+    fprintf(fzout,"runTimeModifiable %s;\n",runTimeModifiable.c_str()); //one less new line since last line
+}
+
+void openFoamPolyMesh::writeFvSchemes()
+{
+    //could possibly write something that is called writeScheme that takes in
+    //the scheme name and an array with what parts of the scheme will be written
+
+    //write the ddtSchemes
+    fprintf(fzout,"ddtSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",ddtSchemes_default.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the gradSchemes
+    fprintf(fzout,"gradSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",gradSchemes_default.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the divSchemes
+    fprintf(fzout,"divSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",divSchemes_default.c_str());
+    fprintf(fzout,"    div(phi,T)      %s;\n",divSchemes_divOfPhiAndT.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the laplacianSchemes
+    fprintf(fzout,"laplacianSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",laplacianSchemes_default.c_str());
+    fprintf(fzout,"    laplacian(DT,T) %s;\n",laplacianSchemes_laplacianOfDTandT.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the interpolationSchemes
+    fprintf(fzout,"interpolationSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",interpolationSchemes_default.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the SnGradSchemes
+    fprintf(fzout,"SnGradSchemes\n{\n");
+    fprintf(fzout,"    default         %s;\n",SnGradSchemes_default.c_str());
+    fprintf(fzout,"    SnGrad(T)       %s;\n",SnGradSchemes_SnGradOfT.c_str());
+    fprintf(fzout,"}\n\n");
+
+    //write the fluxRequired part
+    fprintf(fzout,"fluxRequired\n{\n");
+    fprintf(fzout,"    default         %s;\n",fluxRequired_default.c_str());
+    if(fluxRequired_T == true)
+    {
+        fprintf(fzout,"    T;\n");
+    }
+    fprintf(fzout,"}\n");
+}
+
+void openFoamPolyMesh::writeFvSolution()
+{
+    fprintf(fzout,"solvers\n{\n");
+
+    fprintf(fzout,"    T\n    {\n");
+    fprintf(fzout,"        solver          %s;\n",solvers_T_solver.c_str());
+    fprintf(fzout,"        preconditioner  %s;\n",solvers_T_preconditioner.c_str());
+    fprintf(fzout,"        tolerance       %s;\n",solvers_T_tolerance.c_str());
+    fprintf(fzout,"        relTol          %s;\n",solvers_T_relTol.c_str());
+    fprintf(fzout,"    }\n");
+
+    fprintf(fzout,"}\n\n");
+
+    fprintf(fzout,"SIMPLE\n{\n");
+
+    fprintf(fzout,"    nNonOrthogonalCorrectors %s;\n",SIMPLE_nNonOrthogonalCorrectors.c_str());
+
+    fprintf(fzout,"}\n");
+}
+
+void openFoamPolyMesh::writeSetFieldsDict()
+{
+    fprintf(fzout,"defaultFieldValues\n(\n");
+
+    fprintf(fzout,"    volScalarFieldValue T %s\n",defaultTvalue.c_str());
+    fprintf(fzout,"    volScalarFieldValue source %s\n",defaultSourceValue.c_str());
+
+    fprintf(fzout,");\n\n");
+
+    fprintf(fzout,"regions\n(\n");
+    if(distributionType == "boxToCell")
+    {
+        fprintf(fzout,"    boxToCell\n    {\n");
+        fprintf(fzout,"        box (%s) (%s);\n",boxMinCoordinates.c_str(),boxMaxCoordinates.c_str());
+        fprintf(fzout,"        fieldValues\n        (\n");
+        fprintf(fzout,"            volScalarFieldValue T %s\n",distributionTValue.c_str());
+        fprintf(fzout,"            volScalarFieldValue source %s\n",distributionSourceValue.c_str());
+        fprintf(fzout,"        );\n    }\n");
+    }
+    fprintf(fzout,");\n");
 }
--- src/ninja/openFoamPolyMesh.h
+++ src/ninja/openFoamPolyMesh.h
@@ -87,7 +87,7 @@ public:
     void printBoundaryPatch(std::string patchName,std::string patchType,double nFaces,double startFace,std::string physicalType);  //for printing a patch in the boundaries. I noticed there was a lot of repetition with only one or two small parts changing for each patch and thought it would make a good function.
     void printBoundaries();     //for printing the boundary file, calls printBoundaryPatch();
 
-//set up extra stuff to be moved elsewhere
+//above stuff is technically for the constant directory. This stuff is for the time 0 directory.
 
     //just a way to group a lot of stuff I was doing in single lines over and over. Should make it more legible and shrink the number of lines of code.
     //was having a rough time thinking of a good name so maybe need to change this. Was trying to be consistent with names in openFoam format
@@ -113,7 +113,20 @@ public:
      * looks to be the volumetric flux across faces. Currently not needed, but could be in the future
      * but we need to figure out how to replicate phi from velocities and scalar for this to work correctly
     */
-    void printPhi(element elem);
+    void calculatePhi(element elem);
+
+//now create the stuff for the system directory
+    //creates the controlDict file
+    void writeControlDict();
+
+    //creates the fvSchemes file
+    void writeFvSchemes();
+
+    //creates the fvSolution file
+    void writeFvSolution();
+
+    //creates the setFieldsDict file
+    void writeSetFieldsDict();
 
 private:
 
@@ -157,7 +170,7 @@ private:
 
     wn_3dArray x, y, z;  //the x,y,z values for the mesh
 
-    //values used in other files
+//values used for time directory. Stuff above is for constant directory, but sometimes used for other stuff below as well
     std::string transportPropertiesPath;    //path to the transportProperties file, found in constant from the case directory
     double diffusivityConstant;    //the diffusivity constant
 
@@ -169,6 +182,64 @@ private:
 
     std::string phiPath;    //path to the density phi data file, found in 0 from the case directory
 
+//values used for system directory
+    std::string controlDictPath;    //path to controlDict file
+    std::string fvSchemesPath;      //path to fvSchemes file
+    std::string fvSolutionPath;     //path to fvSolution file
+    std::string setFieldsDictPath;  //path to setFieldsDict file
+
+    //controlDict variables
+    std::string application;        //the simulation type, the application variable in controlDict file
+    std::string startFrom;          //controls the start time of the simulation. Options are: firstTime, startTime, latestTime
+    std::string startTime;          //startTime for the simulation if startFrom is specified to be startTime
+    std::string stopAt;             //controls the end time of the simulation. Options are: endTime, writeNow, noWriteNow, nextWrite
+    std::string endTime;            //end time for the simulation when stopAt is specified to be endTime
+    std::string deltaT;             //time step of the simulation
+    std::string writeControl;       //controls the timing of write output to file. Options are: timeStep, runTime, adjustableRunTime, cpuTime, clockTime
+    std::string writeInterval;      //frequency after startTime for when new time directories are written
+    std::string purgeWrite;         //number of written out time directories to be kept for a given simulation
+    std::string writeFormat;        //specifies the format of the data files. Options are: ascii or binary
+    std::string writePrecision;     //number of decimal places for writing time directories
+    std::string writeCompression;   //compress written out files with gzip yes or no
+    std::string timeFormat;         //choice of name format for the time directories. Options are: fixed, scientific, general
+    std::string timePrecision;      //number of decimal places for the time directory names
+    std::string runTimeModifiable;  //allow controlDict to change, thus changing simulation, in the middle of simulation
+
+    //fvSchemes variables
+    std::string ddtSchemes_default;     //default ddtSchemes: Euler or SteadyState
+    std::string gradSchemes_default;    //default gradSchemes: Gauss linear or cellMDlimited leastSquares 0.5, lots of combinations
+    std::string divSchemes_default;     //default divSchemes: usually set to none or Gauss limitedLinear
+    std::string divSchemes_divOfPhiAndT;    //divSchemes div(phi,T): Gauss limitedLinear 1 or bounded Gauss upwind
+    std::string laplacianSchemes_default;   //default laplacianSchemes: usually set to none or Gauss linear limited
+    std::string laplacianSchemes_laplacianOfDTandT; //laplacianSchemes laplacian(DT,T): usually Gauss linear corrected
+    std::string interpolationSchemes_default;       //default interpolationSchemes: usually linear
+    std::string SnGradSchemes_default;          //default SnGradSchemes: usually corrected
+    std::string SnGradSchemes_SnGradOfT;        //SnGradSchemes SnGrad(T): limited 0.5
+    std::string fluxRequired_default;       //default fluxRequired setting
+    bool fluxRequired_T;         //Write T or not as needing a flux
+
+    //fvSolution variables
+    //note this is trickier than the others because there are so many extra things done for the momentum solver
+    //and the structure is different so will need a lot of work to change stuff here eventually, or just simply write out every single option
+    std::string solvers_T_solver;       //the solver used to solve for T found in the solvers section
+    std::string solvers_T_preconditioner;   //the preconditioner used for T found in the solvers section
+    std::string solvers_T_tolerance;        //the tolerance used to solve for T found in the solvers section
+    std::string solvers_T_relTol;       //the relative tolerance used to solve for T found int he solvers section
+    std::string SIMPLE_nNonOrthogonalCorrectors;    //how to correct for NonOrthogonality
+    //there's a ton more stuff, almost would be better to copy and paste a file that is the same every time
+    //for now, just won't write any of it and hopefully it doesn't change the simulation at all
+
+    //not entirely sure how to do this for other ways to specify values. Right now I'm just writing this
+    //for the current scalar transport case. Will probably want some kind of pointer function so it is easy
+    //to know what to pass in
+    //setFieldsDict variables
+    std::string defaultTvalue;      //default scalar value, so value to be placed everywhere where not specified
+    std::string defaultSourceValue; //default source value, so value to be placed everywhere where not specified
+    std::string distributionType;   //method to be used. If boxToCell, then will use a bounding box for specifying the source
+    std::string boxMinCoordinates;  //set of xmin, ymin, and zmin coordinates for the bounding box if using boxToCell distribution method
+    std::string boxMaxCoordinates;  //set of xmax, ymax, and zmax coordinates for the bounding box if using boxToCell distribution method
+    std::string distributionTValue; //value of scalar in the location to be specified
+    std::string distributionSourceValue;    //value of source in the location to be specified
 
 };
 
