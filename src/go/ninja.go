f91d8ff483e6623dbeaba3ba978bfadd8a946086
Kyle Shannon <kyle@pobox.com>
2017-09-20 15:02:56 -0600

    add comment

--- src/go/ninja.go
+++ src/go/ninja.go
@@ -40,7 +40,7 @@ func arrayToSlice(data *C.double, n C.int) []float64 {
 // breaks between the colors, and the scheme as defined above.  Note if a color
 // scheme is added, it must be added in legend/legend.go and here.
 //
-// See legend.DrawLegend for documention
+// See legend.DrawLegend for documentation
 //export ninja_draw_legend
 func ninja_draw_legend(path *C.char, breaks *C.double, n C.int, scheme C.int, units *C.char) C.int {
 	p := C.GoString(path)
@@ -61,6 +61,10 @@ func ninja_draw_legend(path *C.char, breaks *C.double, n C.int, scheme C.int, un
 	return 0
 }
 
+// ninja_thredds_download fetches a forecast file from the UCAR thredds data
+// server.
+//
+// See thredds.Download for documentation
 //export ninja_thredds_download
 func ninja_thredds_download(path *C.char, name *C.char, minX, maxX, minY, maxY C.double, hours C.int) C.int {
 	p := C.GoString(path)
