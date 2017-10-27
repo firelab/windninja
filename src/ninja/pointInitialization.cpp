8c4456cd47a086e4122b93938ae95ace2d394f68
tfinney9 <tfinney@fs.fed.us>
2017-10-27 09:57:07 -0600

    added knots into station-fetch

--- src/ninja/pointInitialization.cpp
+++ src/ninja/pointInitialization.cpp
@@ -643,6 +643,11 @@ vector<pointInitialization::preInterpolate> pointInitialization::readDiskLine(st
             oStation.speed=dfTempValue;
             oStation.inputSpeedUnits=velocityUnits::kilometersPerHour;
         }
+        else if( EQUAL(pszKey, "kts"))
+        {
+            oStation.speed=dfTempValue;
+            oStation.inputSpeedUnits=velocityUnits::knots;
+        }
         else
         {
             oErrorString = "Invalid units for speed: ";
@@ -2271,7 +2276,7 @@ void pointInitialization::fetchStationData(std::string URL,
         outFile.open(tName.c_str());
         CPLDebug("STATION_FETCH", "%d stations saved to %s", fCount, tName.c_str());
         CPLDebug("STATION_FETCH", "Downloading Data from MesoWest....");
-        std::string header="\"Station_Name\",\"Coord_Sys(PROJCS,GEOGCS)\",\"Datum(WGS84,NAD83,NAD27)\",\"Lat/YCoord\",\"Lon/XCoord\",\"Height\",\"Height_Units(meters,feet)\",\"Speed\",\"Speed_Units(mph,kph,mps)\",\"Direction(degrees)\",\"Temperature\",\"Temperature_Units(F,C)\",\"Cloud_Cover(%)\",\"Radius_of_Influence\",\"Radius_of_Influence_Units(miles,feet,meters,km)\",\"date_time\"";
+        std::string header="\"Station_Name\",\"Coord_Sys(PROJCS,GEOGCS)\",\"Datum(WGS84,NAD83,NAD27)\",\"Lat/YCoord\",\"Lon/XCoord\",\"Height\",\"Height_Units(meters,feet)\",\"Speed\",\"Speed_Units(mph,kph,mps,kts)\",\"Direction(degrees)\",\"Temperature\",\"Temperature_Units(F,C)\",\"Cloud_Cover(%)\",\"Radius_of_Influence\",\"Radius_of_Influence_Units(miles,feet,meters,km)\",\"date_time\"";
         outFile<<header<<endl;
         stationCSVNames.push_back(tName);
         storeFileNames(stationCSVNames);
