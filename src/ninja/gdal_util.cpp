a5042acc096140680edcbd940aa596c22d2658d1
Kyle Shannon <kyle@pobox.com>
2020-07-31 15:12:57 -0600

    ninja: rm debugging trace from previous commit

--- src/ninja/gdal_util.cpp
+++ src/ninja/gdal_util.cpp
@@ -158,6 +158,11 @@ bool GDALGetBounds( GDALDataset *poDS, double *boundsLonLat )
 
     oSourceSRS.importFromWkt( &pszPrj );
     oTargetSRS.SetWellKnownGeogCS( "WGS84" );
+#ifdef GDAL_COMPUTE_VERSION
+#if GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION(3,0,0)
+    oTargetSRS.SetAxisMappingStrategy(OAMS_TRADITIONAL_GIS_ORDER);
+#endif /* GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION(3,0,0) */
+#endif /* GDAL_COMPUTE_VERSION */
 
     poCT = OGRCreateCoordinateTransformation( &oSourceSRS, &oTargetSRS );
     if( poCT == NULL )
@@ -208,6 +213,11 @@ bool GDALTestSRS( GDALDataset *poDS )
 
     oSourceSRS.importFromWkt( &pszPrj );
     oTargetSRS.SetWellKnownGeogCS( "WGS84" );
+#ifdef GDAL_COMPUTE_VERSION
+#if GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION(3,0,0)
+    oTargetSRS.SetAxisMappingStrategy(OAMS_TRADITIONAL_GIS_ORDER);
+#endif /* GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION(3,0,0) */
+#endif /* GDAL_COMPUTE_VERSION */
 
     poCT = OGRCreateCoordinateTransformation( &oSourceSRS, &oTargetSRS );
 
@@ -635,6 +645,7 @@ std::string FetchTimeZone( double dfX, double dfY, const char *pszWkt )
         OGRSpatialReference oSourceSRS, oTargetSRS;
         OGRCoordinateTransformation *poCT;
 
+        // TODO(kyle): check on GDAL 3.x axis ordering here.
         oSourceSRS.SetWellKnownGeogCS( "WGS84" );
         oTargetSRS.importFromWkt( (char**)&pszWkt );
 
