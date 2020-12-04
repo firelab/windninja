a4f021039a608b27924be7c4023bc5e32ec18e7e
Natalie <nwagenbrenner@gmail.com>
2020-12-04 13:35:01 -0700

    set sample interpolation scheme for cfd output based on whether or not the output height is resolved by the cfd mesh, for #392 and #76

--- src/ninja/ninjafoam.cpp
+++ src/ninja/ninjafoam.cpp
@@ -303,6 +303,21 @@ bool NinjaFoam::simulate_wind()
     startOutputSampling = omp_get_wtime();
     #endif
 
+    //Uupdate the sampleDict interpolation scheme. If the the output wind height is not 
+    //resolved (if we are sampling in the lowest cell), then we will use a log
+    //interpolation from the cell-center value in the lowest cell in the mesh. Otherwise,
+    //we use cellPoint for a linear interpolation.
+    std::string scheme;
+    if(CheckIfOutputWindHeightIsResolved()){
+        scheme = "cellPoint";
+    }
+    else{
+        scheme = "cell";
+    }
+    const char *pszInput = CPLFormFilename(pszFoamPath, "system/sampleDict", "");
+    const char *pszOutput = CPLFormFilename(pszFoamPath, "system/sampleDict", "");
+    CopyFile(pszInput, pszOutput, "$interpolationScheme$", scheme);
+
     input.Com->ninjaCom(ninjaComClass::ninjaNone, "Sampling at requested output height...");
     Sample();
     SampleRawOutput();
