94cd9211aad5d1bb0287c5698b554c7256064d34
Natalie <nwagenbrenner@gmail.com>
2021-06-04 10:28:38 -0600

    update variable name checks for newer versions of the HRRR

--- src/ninja/ncepHrrrSurfInitialization.cpp
+++ src/ninja/ncepHrrrSurfInitialization.cpp
@@ -243,13 +243,15 @@ void ncepHrrrSurfInitialization::setSurfaceGrids( WindNinjaInputs &input,
     {
         if(input.ninjaTime == timeList[i])
         {
+            cout<<"input.ninjaTime = "<<input.ninjaTime<<endl;
             for(unsigned int j = 1; j < srcDS->GetRasterCount(); j++)
             { 
                 poBand = srcDS->GetRasterBand( j );
                 gc = poBand->GetMetadataItem( "GRIB_COMMENT" );
                 std::string bandName( gc );
 
-                if( bandName.find( "Temperature [K]" ) != bandName.npos ){
+                if( bandName.find( "Temperature [K]" ) != bandName.npos ||
+                    bandName.find( "Temperature [C]" ) != bandName.npos){
                     gc = poBand->GetMetadataItem( "GRIB_SHORT_NAME" );
                     std::string bandName( gc );
                     if( bandName.find( "2-HTGL" ) != bandName.npos ){
@@ -297,7 +299,8 @@ void ncepHrrrSurfInitialization::setSurfaceGrids( WindNinjaInputs &input,
                 if( bandName.find( "Total cloud cover [%]" ) != bandName.npos ){
                     gc = poBand->GetMetadataItem( "GRIB_SHORT_NAME" );
                     std::string bandName( gc );
-                    if( bandName.find( "0-RESERVED" ) != bandName.npos ){
+                    if( bandName.find( "0-RESERVED" ) != bandName.npos ||
+                        bandName.find( "0-EATM" ) != bandName.npos){
                         bandList.push_back( j );  // Total cloud cover in % 
                         break;
                     }
