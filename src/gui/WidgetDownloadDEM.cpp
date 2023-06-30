32f2e9717464a6c4a54984c40338dbf4308ba829
Natalie <nwagenbrenner@gmail.com>
2023-06-30 15:00:00 -0600

    update dem download button message for Windows

--- src/gui/WidgetDownloadDEM.cpp
+++ src/gui/WidgetDownloadDEM.cpp
@@ -208,6 +208,9 @@ void WidgetDownloadDEM::saveDEM()
     QVariant mbr = wvGoogleMaps->page()->mainFrame()->evaluateJavaScript("mbr()");
     if(mbr.isNull()) {
         qDebug()<<"no mbr";
+        QMessageBox mbox;
+        mbox.setText("DEM bounding box not set. Select the DEM bounding box by using the bounding box drawing tool on the upper right corner of the map, entering a point and radius, or entering the bounding box coordinates.");
+        mbox.exec();
         return;
     }
     QVariantList mbrl = mbr.toList();
@@ -215,7 +218,7 @@ void WidgetDownloadDEM::saveDEM()
     {
         QMessageBox mbox;
         mbox.setText("DEM bounding box not set. Select the DEM bounding box by using the bounding box drawing tool on the upper right corner of the map, entering a point and radius, or entering the bounding box coordinates.");
-            mbox.exec();
+        mbox.exec();
         return;
     }
     for(int i=0; i < mbrl.size(); i++) {
