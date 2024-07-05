012a1f94ce0ce5b148de1bc459e7812848d248c6
sathwikreddy56 <sathwikreddy56@gmail.com>
2024-07-05 16:20:59 -0600

    initial work

---
+++ src/ninja/COP30client.cpp
@@ -0,0 +1,187 @@
+/******************************************************************************
+ *
+ * $Id$
+ *
+ * Project:  WindNinja
+ * Purpose:  Client to download COP30 files
+ * Author:   Natalie Wagenbrenner <nwagenbrenner@gmail.com>
+ *
+ ******************************************************************************
+ *
+ * THIS SOFTWARE WAS DEVELOPED AT THE ROCKY MOUNTAIN RESEARCH STATION (RMRS)
+ * MISSOULA FIRE SCIENCES LABORATORY BY EMPLOYEES OF THE FEDERAL GOVERNMENT
+ * IN THE COURSE OF THEIR OFFICIAL DUTIES. PURSUANT TO TITLE 17 SECTION 105
+ * OF THE UNITED STATES CODE, THIS SOFTWARE IS NOT SUBJECT TO COPYRIGHT
+ * PROTECTION AND IS IN THE PUBLIC DOMAIN. RMRS MISSOULA FIRE SCIENCES
+ * LABORATORY ASSUMES NO RESPONSIBILITY WHATSOEVER FOR ITS USE BY OTHER
+ * PARTIES,  AND MAKES NO GUARANTEES, EXPRESSED OR IMPLIED, ABOUT ITS QUALITY,
+ * RELIABILITY, OR ANY OTHER CHARACTERISTIC.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+
+#include "COP30client.h"
+
+COP30Client::COP30Client() : SurfaceFetch()
+{
+    if(CPLGetConfigOption("CUSTOM_COP30_API_KEY", NULL) != NULL)
+    {
+        APIKey = CPLGetConfigOption("CUSTOM_COP30_API_KEY", NULL);
+        CPLDebug("COP30_CLIENT", "Setting a custom COP30 API key to %s", APIKey);
+    }
+    else if(CPLGetConfigOption("HAVE_CLI_COP30_API_KEY", NULL) != NULL)
+    {
+        APIKey = CPLGetConfigOption("HAVE_CLI_COP30_API_KEY", NULL);
+        CPLDebug("COP30_CLIENT", "Setting CLI COP30 API key to %s", APIKey);
+    }
+    else
+    { 
+        //if a custom key wasn't set, use the one set in cmake_cli.cpp for CLI runs or the defalut key for GUI runs
+        APIKey = CPLGetConfigOption("NINJA_GUI_COP30_API_KEY", NULL);
+        CPLDebug("COP30_CLIENT", "Setting GUI COP30 API key to %s", APIKey);
+    }
+    
+    xRes = 30.0;
+    yRes = 30.0;
+    northeast_x = -180;
+    northeast_y = 60;
+    southeast_x = 180;
+    southeast_y = -56;
+    southwest_x = -180;
+    southwest_y = -56;
+    northwest_x = -180;
+    northwest_y = 60;
+}
+
+COP30Client::~COP30Client()
+{
+}
+
+SURF_FETCH_E COP30Client::FetchBoundingBox( double *bbox, double resolution,
+                                               const char *filename,
+                                               char **options )
+{
+    (void)resolution;
+    if( NULL == filename )
+    {
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    if(APIKey == NULL)
+    {
+        CPLError( CE_Failure, CPLE_AppDefined,
+                  "COP30 download failed. No API key specified.");
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    /*
+    ** We have an arbitrary limit on request size, 0.001 degrees
+    */
+    if( fabs( bbox[0] - bbox[2] ) < 0.001 || fabs( bbox[1] - bbox[3] ) < 0.001 )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined,
+                  "Bounding box too small, must be greater than " \
+                  "0.001 x 0.001 degrees." );
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    //check that we are within the COP30 bounds
+    std::string osDataPath = FindDataPath( "srtm_region.geojson" );
+    osDataPath = osDataPath;
+    const char *pszGeom;
+    pszGeom = CPLSPrintf( "POLYGON((%lf %lf,%lf %lf,%lf %lf,%lf %lf,%lf %lf))",
+                           bbox[1], bbox[0], bbox[3], bbox[0],
+                           bbox[3], bbox[2], bbox[1], bbox[2],
+                           bbox[1], bbox[0] );
+    CPLDebug( "COP30_CLIENT", "Testing if %s contains %s", osDataPath.c_str(),
+              pszGeom );
+
+    if( !NinjaOGRContain( pszGeom, osDataPath.c_str(), "COP30_region" ) )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined,
+                  "Requested DEM is outside of the COP30 bounds." );
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    const char *pszUrl;
+
+    int nEpsgCode = -1;
+    //get UTM zone as EPSG
+    nEpsgCode = BoundingBoxUtm( bbox );
+
+    /*
+    ** Better check?
+    */
+    if( nEpsgCode < 1 )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined, "Invalid EPSG code." );
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    /*-----------------------------------------------------------------------------
+     *  Request a COP30GL1 DEM via the OpenTopography API
+     *-----------------------------------------------------------------------------*/
+    pszUrl = CPLSPrintf( COP30_REQUEST_TEMPLATE, bbox[2], bbox[0], bbox[3], bbox[1], APIKey );
+    psResult = NULL;
+    psResult = CPLHTTPFetch( pszUrl, NULL );
+    CPLDebug( "COP30_CLIENT", "Request URL: %s", pszUrl );
+
+     /*-----------------------------------------------------------------------------
+     *  Check the result of the request
+     *-----------------------------------------------------------------------------*/
+    CPLDebug( "COP30_CLIENT", "Response: %s", psResult->pabyData );
+    if( !psResult || psResult->nStatus != 0 || psResult->nDataLen < 1 || psResult->pszErrBuf != NULL) 
+    {
+        if( strstr( (char*)psResult->pszErrBuf, "HTTP error code : 401" ) )
+        {
+            CPLError( CE_Failure, CPLE_AppDefined,
+                  "Failed to download file, bad API key." );
+        }
+        else
+        {
+            CPLError( CE_Failure, CPLE_AppDefined,
+                      "Failed to download file." );
+        }
+        CPLHTTPDestroyResult( psResult );
+        return SURF_FETCH_E_BAD_INPUT;
+    }
+
+    VSILFILE *fout;
+    fout = VSIFOpenL( "NINJA_COP30.tif", "wb" );
+    if( !fout )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined,
+                  "Failed to open COP30 file for writing." );
+        CPLHTTPDestroyResult( psResult );
+        return SURF_FETCH_E_IO_ERR;
+    }
+    VSIFWriteL( psResult->pabyData, psResult->nDataLen, 1, fout );
+    VSIFCloseL( fout );
+    CPLHTTPDestroyResult( psResult );
+
+     /*-----------------------------------------------------------------------------
+     *  Warp to UTM coords
+     *-----------------------------------------------------------------------------*/
+    CPLDebug( "COP30_CLIENT", "Warping to UTM..." );
+    GDALDatasetH hDS = (GDALDatasetH) GDALOpen("NINJA_COP30.tif", GA_ReadOnly);
+    GDALDatasetH hUtmDS;
+    bool rc = GDALWarpToUtm( filename, hDS, hUtmDS);
+    if(rc != true)
+    {
+        return SURF_FETCH_E_IO_ERR;
+    }
+
+    GDALClose(hDS);
+    GDALClose(hUtmDS);
+    VSIUnlink("NINJA_COP30.tif");
+
+    return SURF_FETCH_E_NONE;
+}
+
---
+++ src/ninja/COP30client.h
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *
+ * $Id$
+ *
+ * Project:  WindNinja
+ * Purpose:  Client to download SRTM files
+ * Author:   Sathwik Reddy Chandiri <sathwikreddy56@gmail.com>
+ *
+ ******************************************************************************
+ *
+ * THIS SOFTWARE WAS DEVELOPED AT THE ROCKY MOUNTAIN RESEARCH STATION (RMRS)
+ * MISSOULA FIRE SCIENCES LABORATORY BY EMPLOYEES OF THE FEDERAL GOVERNMENT
+ * IN THE COURSE OF THEIR OFFICIAL DUTIES. PURSUANT TO TITLE 17 SECTION 105
+ * OF THE UNITED STATES CODE, THIS SOFTWARE IS NOT SUBJECT TO COPYRIGHT
+ * PROTECTION AND IS IN THE PUBLIC DOMAIN. RMRS MISSOULA FIRE SCIENCES
+ * LABORATORY ASSUMES NO RESPONSIBILITY WHATSOEVER FOR ITS USE BY OTHER
+ * PARTIES,  AND MAKES NO GUARANTEES, EXPRESSED OR IMPLIED, ABOUT ITS QUALITY,
+ * RELIABILITY, OR ANY OTHER CHARACTERISTIC.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+#ifndef COP30_CLIENT_H_
+#define COP30_CLIENT_H_
+
+#include <string>
+#include <cstdio>
+
+#include "cpl_http.h"
+
+#include "surface_fetch.h"
+
+/*-----------------------------------------------------------------------------
+ *  REST API", string templates
+ *-----------------------------------------------------------------------------*/
+
+#define COP30_REQUEST_TEMPLATE "https://portal.opentopography.org/" \
+                              "API/globaldem?demtype=COP30&" \
+                              "south=%lf&north=%lf&west=%lf&east=%lf&outputFormat=GTiff&API_Key=%s"
+
+/*-----------------------------------------------------------------------------
+ *  LandFireClient Class Definition
+ *-----------------------------------------------------------------------------*/
+class COP30Client : public SurfaceFetch
+{
+public:
+    SRTMClient();
+    ~SRTMClient();
+    virtual SURF_FETCH_E FetchBoundingBox(double *bbox, double resolution,
+                                          const char *filename, char **options);
+private:
+    SRTMClient( SRTMClient &oOther ) { (void)oOther; }
+
+    CPLHTTPResult *psResult;
+    std::string m_JobId;
+    const char* APIKey;
+};
+
+#endif /* SRTM_CLIENT_H_ */
