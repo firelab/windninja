6430f6e5cdac361e963f789ef7d3fc5c006144d1
masonwillman <masonwillman0@gmail.com>
2024-11-04 15:24:41 -0700

    Updated Ubuntu 22.04 Script
    Fixed the errors that were occuring from the script in the pull request. Additionally, updated existing cmake build commands to use all available processors for faster script running.

--- scripts/build_deps_ubuntu_2204.sh
+++ scripts/build_deps_ubuntu_2204.sh
@@ -1,47 +1,56 @@
 #!/bin/bash
 
-# Adapted versions for Ubuntu 22.04:
+# Set installation directories and versions
 PREFIX=/usr/local
 POPPLER="poppler-22.02.0"
 PROJ="proj-8.2.1"
 GDAL="gdal-3.4.1"
 
-# Dependencies
-sudo apt install libfontconfig1-dev \
-                  libcurl4-gnutls-dev \
-                  libnetcdf-dev \
-                  libboost-program-options-dev \
-                  libboost-date-time-dev \
-                  libgeos-dev \
-                  libboost-test-dev \
-                  libsqlite3-dev \
-                  sqlite3
+# Install necessary dependencies for building Poppler, PROJ, GDAL, and other tools
+sudo apt install -y libfontconfig1-dev \
+                    libcurl4-gnutls-dev \
+                    libnetcdf-dev \
+                    libboost-program-options-dev \
+                    libboost-date-time-dev \
+                    libgeos-dev \
+                    libboost-test-dev \
+                    libsqlite3-dev \
+                    sqlite3 \
+                    libopenjp2-7-dev \
+                    libtiff-dev
+                    
 
-# Get and build poppler for PDF support in GDAL
-wget http://poppler.freedesktop.org/$POPPLER.tar.xz
-tar -xvf $POPPLER.tar.xz
+# Install Poppler for PDF support in GDAL
+wget http://poppler.freedesktop.org/$POPPLER.tar.xz 
+tar -xvf $POPPLER.tar.xz 
 cd $POPPLER/
-./configure --prefix=$PREFIX --enable-xpdf-headers
-make
+mkdir build                         &&
+cd    build                         &&
+cmake  -DCMAKE_BUILD_TYPE=Release   \
+       -DCMAKE_INSTALL_PREFIX=$PREFIX  \
+       -DTESTDATADIR=$PWD/testfiles \
+       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+       ..                           &&
+make -j$(nproc)  # Use all available cores for faster compilation
 sudo make install
-cd ..
+cd ../..
 
-# Get and build proj
-wget http://download.osgeo.org/proj/$PROJ.tar.gz
-tar xvfz $PROJ.tar.gz
+# Install PROJ (a cartographic projection library)
+wget http://download.osgeo.org/proj/$PROJ.tar.gz 
+tar xvfz $PROJ.tar.gz 
 cd $PROJ
 ./configure --prefix=$PREFIX
 make clean
-make
+make -j$(nproc)
 sudo make install
 cd ..
 
-# Get and build GDAL with poppler support
-wget http://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz
-tar -xvf $GDAL.tar.gz
-cd $GDAL/
+# Install GDAL with Poppler support
+wget http://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz 
+tar -xvf $GDAL.tar.gz 
+cd $GDAL/ 
 ./configure --prefix=$PREFIX --with-poppler=$PREFIX
-make -j 8
+make -j$(nproc)
 sudo make install
 cd ..
 
@@ -51,13 +60,13 @@ cd ..
 # !unavailable right now (20240911, ppa:rock-core/qt4)
 # used another, ppa:ubuntuhandbook1/ppa
 
-sudo add-apt-repository ppa:ubuntuhandbook1/ppa
-sudo apt install libqt4-dev libqtwebkit-dev
+sudo add-apt-repository ppa:ubuntuhandbook1/ppa 
+sudo apt install libqt4-dev libqtwebkit-dev 
 
 # Use OpenFOAM 9; OpenFOAM 8 not available for Ubuntu 22.04
 # add the dl.openfoam.org repo and install OpenFOAM 9
-sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key | apt-key add -"
+sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
 sudo add-apt-repository http://dl.openfoam.org/ubuntu
-sudo apt-get update
-sudo apt-get -y install openfoam9
-echo "source /opt/openfoam9/etc/bashrc" >> ~/.bashrc
+sudo apt-get update 
+sudo apt-get -y install openfoam9 
+echo "source /opt/openfoam9/etc/bashrc" >> ~/.bashrc 
