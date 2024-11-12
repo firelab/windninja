686f5844ac2db6ef2f8eff639dc31ac78918ee3f
latwood <pianotocador@gmail.com>
2024-11-12 14:25:23 -0700

    Update build_deps_ubuntu_2204.sh
    cleaned up a bit more of the comments and whitespace, and a few of the commands including the apt-get vs apt stuff. Just need to do one last test of the script on a fresh VM of Ubuntu 22.04, then, unless there are other proposed edits, ready to merge with the qt4guiForUbuntu22o04 branch, replacing the script that I had built using the old poppler and proj dependencies.
    
    Still have the qt4 problems to work out, but from what I can tell everything else is working with the changes that we've made so far.

--- scripts/build_deps_ubuntu_2204.sh
+++ scripts/build_deps_ubuntu_2204.sh
@@ -6,7 +6,7 @@ POPPLER="poppler-22.02.0"
 PROJ="proj-8.2.1"
 GDAL="gdal-3.4.1"
 
-# Install necessary dependencies for building Poppler, PROJ, GDAL, and other tools
+# Install necessary dependencies
 sudo apt install -y libfontconfig1-dev \
                     libcurl4-gnutls-dev \
                     libnetcdf-dev \
@@ -18,26 +18,26 @@ sudo apt install -y libfontconfig1-dev \
                     sqlite3 \
                     libopenjp2-7-dev \
                     libtiff-dev
-                    
+
 
 # Install Poppler for PDF support in GDAL
-wget http://poppler.freedesktop.org/$POPPLER.tar.xz 
-tar -xvf $POPPLER.tar.xz 
+wget http://poppler.freedesktop.org/$POPPLER.tar.xz
+tar -xvf $POPPLER.tar.xz
 cd $POPPLER/
-mkdir build                         &&
-cd    build                         &&
-cmake  -DCMAKE_BUILD_TYPE=Release   \
-       -DCMAKE_INSTALL_PREFIX=$PREFIX  \
-       -DTESTDATADIR=$PWD/testfiles \
-       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-       ..                           &&
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DTESTDATADIR=$PWD/testfiles \
+      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+      ..
 make -j$(nproc)  # Use all available cores for faster compilation
 sudo make install
 cd ../..
 
-# Install PROJ (a cartographic projection library)
-wget http://download.osgeo.org/proj/$PROJ.tar.gz 
-tar xvfz $PROJ.tar.gz 
+# Install PROJ
+wget http://download.osgeo.org/proj/$PROJ.tar.gz
+tar xvfz $PROJ.tar.gz
 cd $PROJ
 ./configure --prefix=$PREFIX
 make clean
@@ -46,9 +46,9 @@ sudo make install
 cd ..
 
 # Install GDAL with Poppler support
-wget http://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz 
-tar -xvf $GDAL.tar.gz 
-cd $GDAL/ 
+wget http://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz
+tar -xvf $GDAL.tar.gz
+cd $GDAL/
 ./configure --prefix=$PREFIX --with-poppler=$PREFIX
 make -j$(nproc)
 sudo make install
@@ -57,16 +57,14 @@ cd ..
 # Add qt4 libs from ppa
 # See here for more info:
 # https://ubuntuhandbook.org/index.php/2020/07/install-qt4-ubuntu-20-04/
-# !unavailable right now (20240911, ppa:rock-core/qt4)
-# used another, ppa:ubuntuhandbook1/ppa
-
-sudo add-apt-repository ppa:ubuntuhandbook1/ppa 
-sudo apt install libqt4-dev libqtwebkit-dev 
+sudo add-apt-repository ppa:ubuntuhandbook1/ppa
+sudo apt update
+sudo apt install -y libqt4-dev libqtwebkit-dev
 
 # Use OpenFOAM 9; OpenFOAM 8 not available for Ubuntu 22.04
 # add the dl.openfoam.org repo and install OpenFOAM 9
 sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
 sudo add-apt-repository http://dl.openfoam.org/ubuntu
-sudo apt-get update 
-sudo apt-get -y install openfoam9 
-echo "source /opt/openfoam9/etc/bashrc" >> ~/.bashrc 
+sudo apt update
+sudo apt install -y openfoam9
+echo "source /opt/openfoam9/etc/bashrc" >> ~/.bashrc
