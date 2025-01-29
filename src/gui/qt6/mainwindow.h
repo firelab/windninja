66063d2e39684057e35d86f0651c4a2877f33cad
Austen Harrell <austen.harrell@ulteig.com>
2025-01-28 19:54:03 -0700

    new qt project seed files

--- src/gui/qt6/mainwindow.h
+++ src/gui/qt6/mainwindow.h
@@ -4,18 +4,19 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
