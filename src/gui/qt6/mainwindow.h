3e9f7f6dbcdcdb90365988f46d131025e03573f5
Austen Harrell <austen.harrell@ulteig.com>
2025-02-11 11:46:09 -0700

    2/11 GUI updates

--- src/gui/qt6/mainwindow.h
+++ src/gui/qt6/mainwindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QResizeEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,12 +16,7 @@ public:
   MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
-protected:
-  void resizeEvent(QResizeEvent *event) override; // Handle window resize to maintain rounded corners
-
 private:
   Ui::MainWindow *ui;
-  void applyRoundedCorners(); // Function to apply rounded corners
 };
-
 #endif // MAINWINDOW_H
