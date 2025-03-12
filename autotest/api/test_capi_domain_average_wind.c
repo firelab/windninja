77198cb6a8fadbd6a12007ca8539cdb44072caf3
masonwillman <masonwillman0@gmail.com>
2025-03-12 11:28:33 -0600

    Added google earth output to domain average wind test

--- autotest/api/test_capi_domain_average_wind.c
+++ autotest/api/test_capi_domain_average_wind.c
@@ -51,8 +51,8 @@ int main()
     /* 
      * Set up domain average run 
      */
+    /* inputs that do not vary among ninjas in an army */
     const char * demFile = "/home/mason/Documents/Git/WindNinja/windninja/autotest/api/data/missoula_valley.tif"; 
-    double outputResolution = 100; 
     const char * initializationMethod = "domain_average";
     const char * meshChoice = "coarse";
     const char * vegetation = "grass";
@@ -62,11 +62,21 @@ int main()
     const char * heightUnits = "m";
     bool momentumFlag = 0; //we're using the conservation of mass solver
     unsigned int numNinjas = 2; //two ninjas in the ninjaArmy
-    
+
     /* inputs that can vary among ninjas in an army */
     const double speedList[2] = {5.5, 5.5};
     const char * speedUnits = "mps";
     const double directionList[2] = {220, 300};
+
+    /* inputs specific to output 
+     * Note: Outputs have default values if inputs are not specified (like resolution)
+     */
+    const double outputResolution = 10.0;
+    const char * units = "m";
+    const double width = 1.0;
+    const char * scaling = "equal_color";
+    const char * outputPath  = "/home/mason/Documents/Git/WindNinja/windninja/autotest/api/data/output";
+    const bool outputFlag = 1;
 
     /* 
      * Create the army
@@ -77,88 +87,119 @@ int main()
         printf("NinjaCreateArmy: ninjaArmy = NULL\n");
     }
 
-    err = NinjaInit(papszOptions); //must be called for any simulation
-    if(err != NINJA_SUCCESS)
-    {
-      printf("NinjaInit: err = %d\n", err);
-    }
-    
     /* 
      * Prepare the army
      */
     for(unsigned int i=0; i<numNinjas; i++)
     {
-        err = NinjaSetCommunication(ninjaArmy, i, comType, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetCommunication: err = %d\n", err);
-        }
-     
-        err = NinjaSetNumberCPUs(ninjaArmy, i, nCPUs, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetNumberCPUs: err = %d\n", err);
-        }
-     
-        err = NinjaSetInitializationMethod(ninjaArmy, i, initializationMethod, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetInitializationMethod: err = %d\n", err);
-        }
-     
-        err = NinjaSetDem(ninjaArmy, i, demFile, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetDem: err = %d\n", err);
-        }
-     
-        err = NinjaSetPosition(ninjaArmy, i, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetPosition: err = %d\n", err);
-        }
-     
-        err = NinjaSetInputWindHeight(ninjaArmy, i, height, heightUnits, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetInputWindHeight: err = %d\n", err);
-        }
-     
-        err = NinjaSetOutputWindHeight(ninjaArmy, i, height, heightUnits, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetOutputWindHeight: err = %d\n", err);
-        }
-     
-        err = NinjaSetOutputSpeedUnits(ninjaArmy, i, speedUnits, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetOutputSpeedUnits: err = %d\n", err);
-        }
-     
-        err = NinjaSetDiurnalWinds(ninjaArmy, i, diurnalFlag, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetDiurnalWinds: err = %d\n", err);
-        }
-     
-        err = NinjaSetUniVegetation(ninjaArmy, i, vegetation, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-          printf("NinjaSetUniVegetation: err = %d\n", err);
-        }
-     
-        err = NinjaSetMeshResolutionChoice(ninjaArmy, i, meshChoice, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-            printf("NinjaSetMeshResolutionChoice: err = %d\n", err);
-        }
-     
-        err = NinjaSetNumVertLayers(ninjaArmy, i, nLayers, papszOptions);
-        if(err != NINJA_SUCCESS)
-        {
-            printf("NinjaSetNumVertLayers: err = %d\n", err);
-        }
+      /* 
+       * Sets Simulation Variables
+       */
+      err = NinjaSetCommunication(ninjaArmy, i, comType, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetCommunication: err = %d\n", err);
+      }
+    
+      err = NinjaSetNumberCPUs(ninjaArmy, i, nCPUs, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetNumberCPUs: err = %d\n", err);
+      }
+    
+      err = NinjaSetInitializationMethod(ninjaArmy, i, initializationMethod, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetInitializationMethod: err = %d\n", err);
+      }
+    
+      err = NinjaSetDem(ninjaArmy, i, demFile, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetDem: err = %d\n", err);
+      }
+    
+      err = NinjaSetPosition(ninjaArmy, i, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetPosition: err = %d\n", err);
+      }
+    
+      err = NinjaSetInputWindHeight(ninjaArmy, i, height, heightUnits, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetInputWindHeight: err = %d\n", err);
+      }
+    
+      err = NinjaSetOutputWindHeight(ninjaArmy, i, height, heightUnits, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetOutputWindHeight: err = %d\n", err);
+      }
+    
+      err = NinjaSetOutputSpeedUnits(ninjaArmy, i, speedUnits, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetOutputSpeedUnits: err = %d\n", err);
+      }
+    
+      err = NinjaSetDiurnalWinds(ninjaArmy, i, diurnalFlag, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetDiurnalWinds: err = %d\n", err);
+      }
+    
+      err = NinjaSetUniVegetation(ninjaArmy, i, vegetation, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+        printf("NinjaSetUniVegetation: err = %d\n", err);
+      }
+    
+      err = NinjaSetMeshResolutionChoice(ninjaArmy, i, meshChoice, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetMeshResolutionChoice: err = %d\n", err);
+      }
+    
+      err = NinjaSetNumVertLayers(ninjaArmy, i, nLayers, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetNumVertLayers: err = %d\n", err);
+      }
+
+      /* 
+       * Sets Output Variables
+       */
+      err = NinjaSetOutputPath(ninjaArmy, i, outputPath, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetOutputPath: err = %d\n", err);
+      }
+
+      err = NinjaSetGoogOutFlag(ninjaArmy, i, outputFlag, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetGoogOutFlag: err = %d\n", err);
+      }
+
+      err = NinjaSetGoogResolution (ninjaArmy, i, outputResolution, units, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetGoogResolution: err = %d\n", err);
+      }
+
+      err = NinjaSetGoogSpeedScaling (ninjaArmy, i, scaling, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetGoogSpeedScaling: err = %d\n", err);
+      }
+
+      err = NinjaSetGoogLineWidth (ninjaArmy, i, width, papszOptions);
+      if(err != NINJA_SUCCESS)
+      {
+          printf("NinjaSetGoogLineWidth: err = %d\n", err);
+      }
+
     }
 
     /* 
@@ -177,7 +218,6 @@ int main()
     const double* outputDirectionGrid = NULL;
     const char* outputGridProjection = NULL;
     const int nIndex = 0;
-    const char* units = "m";
     outputSpeedGrid = NinjaGetOutputSpeedGrid(ninjaArmy, nIndex, papszOptions);
     if( NULL == outputSpeedGrid )
     {
