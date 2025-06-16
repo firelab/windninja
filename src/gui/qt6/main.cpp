c6418fb242fca835ae2af99f628796859b906d8b
masonwillman <masonwillman0@gmail.com>
2025-06-16 14:10:08 -0600

    Added code for splash screen affect after version pop up

--- src/gui/qt6/main.cpp
+++ src/gui/qt6/main.cpp
@@ -2,6 +2,10 @@
 #include "../../ninja/windninja.h"
 #include <QApplication>
 #include <QTimer>
+#include <QSplashScreen>
+#include <QPixmap>
+#include <QGraphicsOpacityEffect>
+#include <QPropertyAnimation>
 // #include "modeldata.h"
 // #include "provider.h"
 #include "controller.h"
@@ -10,17 +14,77 @@
 int main(int argc, char *argv[]) {
 
   QApplication a(argc, argv);
-  MainWindow w;
+  QIcon icon(":/wn-icon.png");
+  QString ver = NINJA_VERSION_STRING;
+  a.setWindowIcon(icon);
+  a.setApplicationName(QString("WindNinja"));
+  a.setApplicationVersion(ver);
 
-  Controller controller(&w);
+  MainWindow* w = nullptr;
+  try {
+    w = new MainWindow;
+  } catch (...) {
+    QMessageBox::critical(nullptr, "Initialization Error",
+                          "WindNinja failed to initialize. Most likely cause is failure to find data "
+                          "dependencies. Try setting the environment variable WINDNINJA_DATA");
+    return 1;
+  }
+
+  Controller controller(w);
 
   // Immediately pull timezone data
-  QTimer::singleShot(0, &w, &MainWindow::timeZoneDataRequest);
+  QTimer::singleShot(0, w, &MainWindow::timeZoneDataRequest);
 
-  QIcon icon(":/wn-icon.png");
-  a.setWindowIcon(icon);
-  w.setWindowIcon(icon);
+  // Load and scale splash image
+  QPixmap bigPixmap(":/wn-splash.png");
+  QSize splashSize(1200, 320);
+  QPixmap splashPixmap = bigPixmap.scaled(splashSize, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
 
-  w.show();
+  // Create splash screen
+  QSplashScreen* splash = new QSplashScreen(splashPixmap);
+  splash->setFont(QFont("Sans Serif", 10));
+  QRect screenGeometry = QGuiApplication::primaryScreen()->geometry();
+  QPoint center = screenGeometry.center() - splash->rect().center();
+  splash->move(center);
+
+  // Apply opacity effect
+  QGraphicsOpacityEffect* effect = new QGraphicsOpacityEffect(splash);
+  splash->setGraphicsEffect(effect);
+  effect->setOpacity(0.0); // Initial opacity 0
+
+  // Show splash screen
+  splash->show();
+  a.processEvents(); // Ensure splash is shown before animating
+
+  // Fade-in animation
+  QPropertyAnimation* fadeIn = new QPropertyAnimation(effect, "opacity");
+  fadeIn->setDuration(1500);
+  fadeIn->setStartValue(0.0);
+  fadeIn->setEndValue(1.0);
+
+  // Fade-out animation
+  QPropertyAnimation* fadeOut = new QPropertyAnimation(effect, "opacity");
+  fadeOut->setDuration(1500);
+  fadeOut->setStartValue(1.0);
+  fadeOut->setEndValue(0.0);
+
+  // Sequence: After fade-in completes, wait a bit, then start fade-out
+  QObject::connect(fadeIn, &QPropertyAnimation::finished, [=]() {
+    QTimer::singleShot(1500, [=]() {
+      fadeOut->start(QAbstractAnimation::DeleteWhenStopped);
+    });
+  });
+
+  // After fade-out completes, show main window and clean up
+  QObject::connect(fadeOut, &QPropertyAnimation::finished, [=]() {
+    splash->finish(w);
+    w->show();
+    splash->deleteLater();
+  });
+
+  // Start fade-in
+  fadeIn->start(QAbstractAnimation::DeleteWhenStopped);
+
   return a.exec();
+
 }
