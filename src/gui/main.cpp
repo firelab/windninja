1aa69c97a0ae64348e6afaf275fb6f2b8868bb0d
masonwillman <masonwillman0@gmail.com>
2025-06-18 14:21:39 -0600

    Removed netcdf locking from GUI

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -7,27 +7,10 @@
 #include "controller.h"
 
 
-#ifdef _OPENMP
-omp_lock_t netCDF_lock;
-#endif
 
 int main(int argc, char *argv[]) {
 
-  int result;
-  #ifdef _OPENMP
-  omp_init_lock (&netCDF_lock);
-  #endif
 
-  char ** papszOptions = NULL;
-  NinjaErr err = 0;
-  err = NinjaInit(papszOptions);
-  if(err != NINJA_SUCCESS)
-  {
-    printf("NinjaInit: err = %d\n", err);
-    #ifdef _OPENMP
-    omp_destroy_lock (&netCDF_lock);
-    #endif
-  }
 
   QApplication a(argc, argv);
   MainWindow w;
@@ -38,11 +21,6 @@ int main(int argc, char *argv[]) {
   QTimer::singleShot(0, &w, &MainWindow::timeZoneDataRequest);
 
   w.show();
-  result = a.exec();
 
-  #ifdef _OPENMP
-  omp_destroy_lock (&netCDF_lock);
-  #endif
-
-  return result;
+  return a.exec();
 }
