8002bf4446b3944e3f8af961028edf27a031aa65
latwood <pianotocador@gmail.com>
2025-11-25 02:39:34 -0700

    tested and updated the build_deps_ubuntu_2404.sh build script, while attempting to compile OpenFOAM 8 and 9 from source on Ubuntu 24.04
    bad luck with OpenFOAM 8 and 9 on Ubuntu 24.04, they compiled fine, but they had weird runtime IO errors that implied the need to use an older compiler. I found a workaround, described in the script, I went to the .deb package from the openfoam website, downloaded it, and unpacked it, that worked better so far as I can tell.
    
    compiling gdal required "--with-unix-stdio-64=no", some kind of int64 vs int32 problem. Supposedly another workaround might be "sudo apt install g++-multilib gcc-multilib", but it still needs investigated and tested as a method.

--- scripts/build_deps_ubuntu_2404.sh
+++ scripts/build_deps_ubuntu_2404.sh
@@ -3,40 +3,48 @@
 #This is a limited build without GUI and without OpenFOAM
 #GUI and OpenFOAM support to be added soon
 PREFIX=/usr/local
-POPPLER="poppler-0.23.4"
+POPPLER="poppler-22.02.0"
 PROJ="proj-9.5.1"
-GDAL="gdal-3.2.1"
+GDAL="gdal-3.4.1"
 
 #Dependencies
 sudo -E apt install -y pkg-config libfontconfig1-dev libcurl4-gnutls-dev libnetcdf-dev \
                        libboost-program-options-dev libboost-date-time-dev libgeos-dev \
-                       libboost-test-dev libsqlite3-dev sqlite3 libgeotiff-dev
+                       libboost-test-dev libsqlite3-dev sqlite3 libgeotiff-dev libopenjp2-7-dev
 
-#Get and build poppler for PDF support in GDAL
+# Install Poppler for PDF support in GDAL
 wget https://poppler.freedesktop.org/$POPPLER.tar.xz
-tar -xvf $POPPLER.tar.xz 
+tar -xvf $POPPLER.tar.xz
 cd $POPPLER/
-./configure --prefix=$PREFIX --enable-xpdf-headers
-make
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DTESTDATADIR=$PWD/testfiles \
+      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+      ..
+make -j$(nproc)  # Use all available cores for faster compilation
 sudo make install
-cd ..
+cd ../..
 
-#Get and build proj
+# Install PROJ
 wget https://download.osgeo.org/proj/$PROJ.tar.gz
 tar xvfz $PROJ.tar.gz
 cd $PROJ
 mkdir build && cd build
-cmake ..
-cmake --build .
-sudo cmake --build . --target install
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      ..
+make -j$(nproc)  # Use all available cores for faster compilation
+sudo make install
 cd ../..
 
-#Get and build GDAL with poppler support
+# Install GDAL with Poppler support
 wget https://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz
 tar -xvf $GDAL.tar.gz 
 cd $GDAL/
-./configure --prefix=$PREFIX --with-poppler=$PREFIX
-make -j 8
+./configure --prefix=$PREFIX --with-poppler=$PREFIX --with-unix-stdio-64=no
+make -j$(nproc)
 sudo make install
 cd ..
 
