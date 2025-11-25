1034093945a3e5a69ece6d36c881c1c44e857acf
latwood <pianotocador@gmail.com>
2025-11-25 02:41:54 -0700

    oops, forgot to save the added comments to my last commit, fixed now

--- scripts/build_deps_ubuntu_2404.sh
+++ scripts/build_deps_ubuntu_2404.sh
@@ -48,3 +48,16 @@ make -j$(nproc)
 sudo make install
 cd ..
 
+## bad luck trying to build OpenFOAM 8 and 9 from source, they compiled fine, but they had weird runtime IO errors that imply the need to build with an older compiler
+## https://openfoam.org/download/8-source/, https://openfoam.org/download/9-source/
+## I did find a workaround though: went to the .deb package from the openfoam website, downloaded it, and unpacked it.
+##  https://dl.openfoam.org/ubuntu/dists/focal/main/binary-amd64/openfoam9_20211122_amd64.deb
+##  dpkg-deb -x openfoam9_20211122_amd64.deb .
+##  sudo mv opt/openfoam9/ /opt/
+##  source /opt/openfoam9/etc/bashrc   <-- I usually put this in my /home/<usr>/.bashrc, then "source /home/<usr>/.bashrc"
+##
+## Note, to get the dependencies, I did the following, from https://openfoam.org/download/source/software-for-compilation/
+##  sudo apt-get install build-essential cmake git ca-certificates flex
+##  sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
+##  sudo add-apt-repository http://dl.openfoam.org/ubuntu
+##  sudo apt install openfoam-nopv-deps   <--- well, technically I tried the full paraview "openfoam-deps" version, alongside "paraview-dev", but in theory just the no-paraview version should be enough of the dependencies.
