049b6c0af927e2e5ff93758fb3cfa3236143b6e5
latwood <pianotocador@gmail.com>
2026-04-30 17:15:00 -0600

    attempt at making the tile server hold the SQLite .mbtiles file open for the various tiling fetches rather than constantly opening and closing the SQLite .mbtiles file. Should have made things a bit faster, but there is also a chance that the method is still not quite right. For issue #753.

--- src/gui/tileServer.h
+++ src/gui/tileServer.h
@@ -21,6 +21,7 @@ class TileServer : public QObject
 
 public:
     TileServer(QObject* parent = nullptr);
+    ~TileServer();
 
     bool start(quint16 port = 0); // 0 = auto-pick port
     quint16 port() const;  // no longer needed, baseUrl() returns with this already in it, shared across each dataset
@@ -37,8 +38,8 @@ private:
     QHttpServer m_server;
     quint16 m_port;
     QMap<QString, QString> m_datasetMap;
+    QMap<QString, sqlite3*> m_dbMap;
 
-    QByteArray getTile(int z, int x, int y);
 };
 
 #endif // TILESERVER_H
