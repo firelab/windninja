78d55d53e9ecfd48dc47f5794d78d81ddfe3848b
masonwillman <masonwillman0@gmail.com>
2026-07-07 13:02:42 -0600

    Custom spin box widget also ensures enter/return moves focus to the next child (how pressing tab would work)

--- src/gui/noWheelDoubleSpinBox.h
+++ src/gui/noWheelDoubleSpinBox.h
@@ -6,19 +6,34 @@
 #include <QSpinBox>
 #include <QWheelEvent>
 
-class NoWheelDoubleSpinBox : public QDoubleSpinBox {
+class NoWheelDoubleSpinBox : public QDoubleSpinBox
+{
     Q_OBJECT
+
 public:
     explicit NoWheelDoubleSpinBox(QWidget *parent = nullptr)
         : QDoubleSpinBox(parent) {}
 
 protected:
-    void wheelEvent(QWheelEvent *event) override {
-        if (!hasFocus()) {
+    void wheelEvent(QWheelEvent *event) override
+    {
+        if (!hasFocus())
+        {
             event->ignore();
             return;
         }
         QDoubleSpinBox::wheelEvent(event);
+    }
+
+    void keyPressEvent(QKeyEvent *event) override
+    {
+        if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter)
+        {
+            focusNextChild();
+            return;
+        }
+
+        QDoubleSpinBox::keyPressEvent(event);
     }
 };
 
