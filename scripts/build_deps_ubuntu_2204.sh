e37e194aef4ee76ed1193780ece3c7ea46e27e69
masonwillman <masonwillman0@gmail.com>
2026-07-10 09:32:37 -0600

    Updated build script to use apt get libs

--- scripts/build_deps_ubuntu_2204.sh
+++ scripts/build_deps_ubuntu_2204.sh
@@ -1,61 +1,33 @@
 #!/bin/bash
 
-# Set installation directories and versions
-PREFIX=/usr/local
-POPPLER="poppler-22.02.0"
-PROJ="proj-8.2.1"
-GDAL="gdal-3.4.1"
-
 # Install necessary dependencies
-sudo apt install -y libfontconfig1-dev \
-                    libcurl4-gnutls-dev \
-                    libnetcdf-dev \
-                    libboost-program-options-dev \
-                    libboost-date-time-dev \
-                    libgeos-dev \
-                    libboost-test-dev \
-                    libsqlite3-dev \
-                    sqlite3 \
-                    libopenjp2-7-dev \
-                    libtiff-dev \
-                    libshp-dev
-
-# Install qt6 libs
-##sudo apt install qt6-base-dev qt6-base-dev-tools qt6-webengine-dev qt6-webengine-dev-tools libqt6webenginecore6-bin
-
-# Install Poppler for PDF support in GDAL
-wget https://poppler.freedesktop.org/$POPPLER.tar.xz
-tar -xvf $POPPLER.tar.xz
-cd $POPPLER/
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DTESTDATADIR=$PWD/testfiles \
-      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-      ..
-make -j$(nproc)  # Use all available cores for faster compilation
-sudo make install
-cd ../..
-
-# Install PROJ
-wget https://download.osgeo.org/proj/$PROJ.tar.gz
-tar xvfz $PROJ.tar.gz
-cd $PROJ
-./configure --prefix=$PREFIX
-make clean
-make -j$(nproc)
-sudo make install
-cd ..
-
-# Install GDAL with Poppler support
-wget https://download.osgeo.org/gdal/${GDAL:5}/$GDAL.tar.gz
-tar -xvf $GDAL.tar.gz
-cd $GDAL/
-./configure --prefix=$PREFIX --with-poppler=$PREFIX
-make -j$(nproc)
-sudo make install
-cd ..
+sudo apt install -y \
+    build-essential \
+    cmake \
+    wget \
+    pkg-config \
+    libfontconfig1-dev \
+    libcurl4-gnutls-dev \
+    libnetcdf-dev \
+    libboost-program-options-dev \
+    libboost-date-time-dev \
+    libboost-test-dev \
+    libgeos-dev \
+    libsqlite3-dev \
+    sqlite3 \
+    libopenjp2-7-dev \
+    libtiff-dev \
+    libshp-dev \
+    libpoppler-dev \
+    libproj-dev \
+    proj-bin \
+    proj-data \
+    libgdal-dev \
+    gdal-bin \
+    qt6-base-dev \
+    qt6-base-dev-tools \
+    qt6-webengine-dev \
+    qt6-webengine-dev-tools
 
 # Use OpenFOAM 9; OpenFOAM 8 not available for Ubuntu 22.04
 # add the dl.openfoam.org repo and install OpenFOAM 9
