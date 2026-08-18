792fe6c699210cea8fe274018a6e62f2cd4934a8
latwood <pianotocador@gmail.com>
2026-08-18 15:19:55 -0600

    making a copy of build_deps_ubuntu_2404.sh to start using for Ubuntu 26.04 builds. For issue #804.

---
+++ scripts/build_deps_ubuntu_2604.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install necessary dependencies
+sudo apt install -y pkg-config \
+                    libboost-program-options-dev \
+                    libboost-date-time-dev \
+                    libboost-test-dev \
+                    libshp-dev
+
+# Install qt6 
+sudo apt install -y qt6-base-dev qt6-webengine-dev
+
+# Install GDAL
+sudo apt install -y libgdal-dev
+
+# Install hwloc, for OpenFOAM
+sudo apt install -y libhwloc-dev
+
+# Use OpenFOAM 11; OpenFOAM 9 not available for Ubuntu 24.04
+# add the dl.openfoam.org repo and install OpenFOAM 11
+sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
+sudo add-apt-repository -y http://dl.openfoam.org/ubuntu
+sudo apt update
+sudo apt install -y openfoam11
+echo "source /opt/openfoam11/etc/bashrc" >> ~/.bashrc
