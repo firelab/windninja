a247d9827cb688cbd195d777e12510f52daab687
masonwillman <masonwillman0@gmail.com>
2026-08-26 15:24:34 -0600

    Ensured UTC timezone is set for station time

--- src/gui/pointInitializationInput.cpp
+++ src/gui/pointInitializationInput.cpp
@@ -608,6 +608,8 @@ void PointInitializationInput::pointInitializationTreeViewItemSelectionChanged(c
             {
                 QDateTime startUtcDateTime = QDateTime::fromString(startDateTimeStr, "yyyy-MM-dd'T'HH:mm:ss'Z'");
                 QDateTime stopUtcDateTime = QDateTime::fromString(stopDateTimeStr, "yyyy-MM-dd'T'HH:mm:ss'Z'");
+                startUtcDateTime.setTimeZone(QTimeZone("UTC"));
+                stopUtcDateTime.setTimeZone(QTimeZone("UTC"));
 
                 if (!startUtcDateTime.isValid() || !stopUtcDateTime.isValid())
                 {
@@ -714,13 +716,10 @@ void PointInitializationInput::readStationTime(QDateTime startUtcDateTime, QDate
     ui->weatherStationDataStartDateTimeEdit->setDisplayFormat("MM/dd/yyyy HH:mm");
     ui->weatherStationDataEndDateTimeEdit->setDisplayFormat("MM/dd/yyyy HH:mm");
 
-    QDateTime startDisplay(minStationLocalDateTime.date(), minStationLocalDateTime.time(), Qt::LocalTime);
-    QDateTime endDisplay(maxStationLocalDateTime.date(), maxStationLocalDateTime.time(), Qt::LocalTime);
-
-    ui->weatherStationDataStartDateTimeEdit->setDateTimeRange(startDisplay, endDisplay);
-    ui->weatherStationDataEndDateTimeEdit->setDateTimeRange(startDisplay, endDisplay);
-    ui->weatherStationDataStartDateTimeEdit->setDateTime(startDisplay);
-    ui->weatherStationDataEndDateTimeEdit->setDateTime(endDisplay);
+    ui->weatherStationDataStartDateTimeEdit->setDateTimeRange(minStationLocalDateTime, maxStationLocalDateTime);
+    ui->weatherStationDataEndDateTimeEdit->setDateTimeRange(minStationLocalDateTime, maxStationLocalDateTime);
+    ui->weatherStationDataStartDateTimeEdit->setDateTime(minStationLocalDateTime);
+    ui->weatherStationDataEndDateTimeEdit->setDateTime(maxStationLocalDateTime);
 
     ui->weatherStationDataStartDateTimeEdit->setEnabled(true);
     ui->weatherStationDataEndDateTimeEdit->setEnabled(true);
