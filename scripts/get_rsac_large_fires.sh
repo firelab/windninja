d55169194e80257cb14215e63f362a02d3553c04
masonwillman <masonwillman0@gmail.com>
2026-09-03 11:59:31 -0600

    Removed additional script files

--- scripts/get_rsac_large_fires.sh
+++
@@ -1,2 +0,0 @@
-python ninjahotspots.py --remove-AOI false --remove-footprints false --move-legend false --move-logo false  --scale 1.0 http://activefiremaps.fs.fed.us/data/kml/conus_lg_incidents.kmz large_fires_rsac.kmz
-
